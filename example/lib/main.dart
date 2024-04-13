210ad142d739ca8662e0437f31ce0599dd4478b6
noboru-i <ishikura.noboru@gmail.com>
2024-04-13 16:07:25 +0900

    impl example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  final _flutterImaSdkSamplePlugin = FlutterImaSdkSample();
+  final _api = IMAPlayerApi();
 
   @override
   void initState() {
@@ -25,21 +25,16 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _flutterImaSdkSamplePlugin.getPlatformVersion() ?? 'Unknown platform version';
+      final result =
+          await _api.initialize(InitializeParam(dummy: 'dummy text1'));
+      platformVersion = result.dummy;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
