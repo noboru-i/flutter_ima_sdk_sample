cac947fc6ae19264978792ac762cc7bf4ad2b9b0
noboru-i <ishikura.noboru@gmail.com>
2024-04-13 14:59:40 +0900

    format code on Xcode

--- ios/Classes/FlutterImaSdkSamplePlugin.swift
+++ ios/Classes/FlutterImaSdkSamplePlugin.swift
@@ -2,18 +2,19 @@ import Flutter
 import UIKit
 
 public class FlutterImaSdkSamplePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_ima_sdk_sample", binaryMessenger: registrar.messenger())
-    let instance = FlutterImaSdkSamplePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_ima_sdk_sample", binaryMessenger: registrar.messenger())
+        let instance = FlutterImaSdkSamplePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
+
